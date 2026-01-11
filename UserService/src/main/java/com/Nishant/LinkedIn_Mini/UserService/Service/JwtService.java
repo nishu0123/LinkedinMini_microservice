e933002dd0dc7cf042f35a3cd693b5b2fb6b46e3
nishant <kumarnishu574@gmail.com>
2026-01-12 00:12:58 +0530

    Added Jwt Service to handle the token

--- UserService/src/main/java/com/Nishant/LinkedIn_Mini/UserService/Service/JwtService.java
+++ UserService/src/main/java/com/Nishant/LinkedIn_Mini/UserService/Service/JwtService.java
@@ -1,4 +1,87 @@
 package com.Nishant.LinkedIn_Mini.UserService.Service;
 
+
+import com.Nishant.LinkedIn_Mini.UserService.Entity.UserEntity;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
 public class JwtService {
+
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    // 🔹 TOKEN CREATION
+    public String generateAccessToken(Long userId, String username, String role) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("userId", userId)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + + 15 * 60 * 1000)) // 15 min
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    //Create Refresh Token
+    public String generateRefreshToken(UserEntity user) {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+
+    // 🔹 CLAIMS EXTRACTION
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public Long extractUserId(String token) {
+        return extractAllClaims(token).get("userId", Long.class);
+    }
+
+    public String extractUserRole(String token) {
+        return extractAllClaims(token).get("role", String.class);
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    // 🔹 VALIDATION
+    private boolean isTokenExpired(String token) {
+        return extractAllClaims(token)
+                .getExpiration()
+                .before(new Date());
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            return !isTokenExpired(token);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
 }
+
