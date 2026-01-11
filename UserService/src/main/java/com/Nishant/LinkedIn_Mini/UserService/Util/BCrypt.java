bf9715c73595c13227aee6ae7f3872fcf8cb7eba
nishant <kumarnishu574@gmail.com>
2026-01-12 00:06:50 +0530

    This class is just a wrapper for password encoder and password match

--- UserService/src/main/java/com/Nishant/LinkedIn_Mini/UserService/Util/BCrypt.java
+++ UserService/src/main/java/com/Nishant/LinkedIn_Mini/UserService/Util/BCrypt.java
@@ -1,4 +1,28 @@
 package com.Nishant.LinkedIn_Mini.UserService.Util;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BCrypt {
+
+
+    private final PasswordEncoder passwordEncoder;
+    BCrypt(PasswordEncoder passwordEncoder)
+    {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public String hash(String password)
+    {
+        return passwordEncoder.encode(password);
+    }
+
+    public boolean match(String passwordPlainText , String encodedPassword)
+    {
+        CharSequence rawPassword = passwordPlainText;
+        return passwordEncoder.matches (rawPassword ,  encodedPassword);
+    }
 }
