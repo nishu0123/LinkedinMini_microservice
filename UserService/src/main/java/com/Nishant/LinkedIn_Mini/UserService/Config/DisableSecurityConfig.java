ace743b33802a25bda69e076548380ee7a052384
nishant <kumarnishu574@gmail.com>
2026-01-12 00:10:27 +0530

    Added DisableSecurityConfig class to disable the blocking of api by security , as in userService at this time i want to only encode , match password and return the jwt token .Other Responsibility of authentication and authorization will be given to the Api-gateway

---
+++ UserService/src/main/java/com/Nishant/LinkedIn_Mini/UserService/Config/DisableSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.Nishant.LinkedIn_Mini.UserService.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DisableSecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
+
