55d7f4779ab09e3d1ff159cf372584092e3db766
nishant <kumarnishu574@gmail.com>
2026-01-19 01:44:51 +0530

    initial versio of uploader service

---
+++ UploaderService/src/test/java/com/Nishant/LinkedIn_Mini/UploaderService/UploaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nishant.LinkedIn_Mini.UploaderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploaderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
