6b12e8eb954613dab997294c2c995f5d3d28bd1d
nishant <kumarnishu574@gmail.com>
2026-02-11 01:51:06 +0530

    added configuration for multipart as multipartfile will send through feign

--- PostService/src/main/java/com/Nishant/LinkedIn_Mini/PostService/Config/FeignMultipartConfig.java
+++ PostService/src/main/java/com/Nishant/LinkedIn_Mini/PostService/Config/FeignMultipartConfig.java
@@ -1,4 +1,32 @@
 package com.Nishant.LinkedIn_Mini.PostService.Config;
 
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.http.converter.autoconfigure.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.FeignHttpMessageConverters;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class FeignMultipartConfig {
+
+    private final ObjectProvider<FeignHttpMessageConverters> messageConverters;
+
+    public FeignMultipartConfig(ObjectProvider<FeignHttpMessageConverters> messageConverters) {
+        this.messageConverters = messageConverters;
+    }
+
+    @Bean
+    public Encoder feignFormEncoder() {
+        // We wrap the standard SpringEncoder inside the SpringFormEncoder
+        // This allows the client to handle BOTH JSON and Multipart files
+        return new SpringFormEncoder(new SpringEncoder(messageConverters));
+    }
+    @Bean
+    public feign.Logger.Level feignLoggerLevel() {
+        return feign.Logger.Level.FULL; // Logs the full body and headers
+    }
 }
