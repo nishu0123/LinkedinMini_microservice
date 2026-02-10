6bccc506164f703808ff5414260ec08cd0281e29
nishant <kumarnishu574@gmail.com>
2026-02-11 01:48:24 +0530

    added Feign to upload the post to cloudinary by calling api of uploader service from postService using this Feign

--- PostService/src/main/java/com/Nishant/LinkedIn_Mini/PostService/FeignClient/imageUploaderFeign.java
+++ PostService/src/main/java/com/Nishant/LinkedIn_Mini/PostService/FeignClient/imageUploaderFeign.java
@@ -1,4 +1,23 @@
 package com.Nishant.LinkedIn_Mini.PostService.FeignClient;
 
+import com.Nishant.LinkedIn_Mini.PostService.Config.FeignMultipartConfig;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+@FeignClient(
+        name = "UploaderService",
+        configuration = FeignMultipartConfig.class // CRITICAL: Point to your config
+//        url = "http://localhost:8083" // OR Eureka name
+)
+
 public interface imageUploaderFeign {
+//    @PostMapping("/file")
+// ADD THIS: consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    @PostMapping(value = "/upload/file/uploadImage", consumes = org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE)
+    ResponseEntity<String> upload(@RequestPart("file") MultipartFile file);
 }
