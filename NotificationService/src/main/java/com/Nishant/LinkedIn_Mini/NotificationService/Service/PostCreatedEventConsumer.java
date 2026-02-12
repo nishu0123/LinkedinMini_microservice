329592568b04a7780d12c7bd543222588f58c98d
nishant <kumarnishu574@gmail.com>
2026-02-12 21:21:19 +0530

    consume post created event

--- NotificationService/src/main/java/com/Nishant/LinkedIn_Mini/NotificationService/Service/PostCreatedEventConsumer.java
+++ NotificationService/src/main/java/com/Nishant/LinkedIn_Mini/NotificationService/Service/PostCreatedEventConsumer.java
@@ -1,4 +1,18 @@
 package com.Nishant.LinkedIn_Mini.NotificationService.Service;
 
+import com.Nishant.LinkedIn_Mini.NotificationService.Dto.EventDto.PostCreatedEventDto;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class PostCreatedEventConsumer {
+
+    @KafkaListener(topics = "post-created-topic", groupId = "notification-group")
+    public void consumePostEvent(PostCreatedEventDto event) {
+        System.out.println("New post by: " + event.getUserId());
+        System.out.println("Image URL: " + event.getImageUrl());
+
+        // Logic to find followers and send emails/push notifications
+    }
 }
