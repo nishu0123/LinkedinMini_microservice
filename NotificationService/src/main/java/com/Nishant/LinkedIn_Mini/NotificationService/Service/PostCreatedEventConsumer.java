113321b5e4efc6a6c2a98c77f7813fdbfddf9c2c
nishant <kumarnishu574@gmail.com>
2026-02-12 23:13:53 +0530

    added logic to get the list of followers of user

--- NotificationService/src/main/java/com/Nishant/LinkedIn_Mini/NotificationService/Service/PostCreatedEventConsumer.java
+++ NotificationService/src/main/java/com/Nishant/LinkedIn_Mini/NotificationService/Service/PostCreatedEventConsumer.java
@@ -1,12 +1,27 @@
 package com.Nishant.LinkedIn_Mini.NotificationService.Service;
 
 import com.Nishant.LinkedIn_Mini.NotificationService.Dto.EventDto.PostCreatedEventDto;
+import com.Nishant.LinkedIn_Mini.NotificationService.Dto.FeignDto.PersonDto;
+import com.Nishant.LinkedIn_Mini.NotificationService.FeignClient.GetFollowerFeign;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
+import static java.lang.System.in;
+
+
+@Slf4j
 @Service
 public class PostCreatedEventConsumer {
+
+    private final GetFollowerFeign getFollowerFeign;
+
+    public PostCreatedEventConsumer(GetFollowerFeign getFollowerFeign) {
+        this.getFollowerFeign = getFollowerFeign;
+    }
 
     @KafkaListener(topics = "post-created-topic", groupId = "notification-group")
     public void consumePostEvent(PostCreatedEventDto event) {
@@ -14,5 +29,18 @@ public class PostCreatedEventConsumer {
         System.out.println("Image URL: " + event.getImageUrl());
 
         // Logic to find followers and send emails/push notifications
+
+
+        //now call the connection service to get the first-degree connection and send the email to all the user
+        List<PersonDto> followersList =  getFollowerFeign.getFirstDegreeConnection(event.getUserId() , event.getUserId().toString());
+
+        //now produce an event followerInfoReceivedToSendMailOrPushNotification from notification service
+        //and notification service will again consume the same event to send the mail
+
+        for(int i = 0; i<followersList.size(); i++)
+        {
+            PersonDto tempval = followersList.get(i); //tempval will store the follwers details
+            log.info("follower " + i+1 + " = " + tempval);
+        }
     }
 }
