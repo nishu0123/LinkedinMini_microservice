708a754d98d8df87a043f501b44e262373fd316a
nishant <kumarnishu574@gmail.com>
2026-02-14 00:59:38 +0530

    added logic to get the followers of user who created post

--- NotificationService/src/main/java/com/Nishant/LinkedIn_Mini/NotificationService/Service/PostCreatedEventConsumer.java
+++ NotificationService/src/main/java/com/Nishant/LinkedIn_Mini/NotificationService/Service/PostCreatedEventConsumer.java
@@ -1,6 +1,7 @@
 package com.Nishant.LinkedIn_Mini.NotificationService.Service;
 
 import com.Nishant.LinkedIn_Mini.NotificationService.Dto.EventDto.PostCreatedEventDto;
+import com.Nishant.LinkedIn_Mini.NotificationService.Dto.EventDto.SendNotificationEventDto;
 import com.Nishant.LinkedIn_Mini.NotificationService.Dto.FeignDto.PersonDto;
 import com.Nishant.LinkedIn_Mini.NotificationService.FeignClient.GetFollowerFeign;
 import lombok.extern.slf4j.Slf4j;
@@ -19,9 +20,13 @@ public class PostCreatedEventConsumer {
 
     private final GetFollowerFeign getFollowerFeign;
 
-    public PostCreatedEventConsumer(GetFollowerFeign getFollowerFeign) {
+    private final  SendNotificationEventProducer sendNotificationEventProducer;
+
+    public PostCreatedEventConsumer(GetFollowerFeign getFollowerFeign, SendNotificationEventProducer sendNotificationEventProducer) {
         this.getFollowerFeign = getFollowerFeign;
+        this.sendNotificationEventProducer = sendNotificationEventProducer;
     }
+
 
     @KafkaListener(topics = "post-created-topic", groupId = "notification-group")
     public void consumePostEvent(PostCreatedEventDto event) {
@@ -39,8 +44,24 @@ public class PostCreatedEventConsumer {
 
         for(int i = 0; i<followersList.size(); i++)
         {
-            PersonDto tempval = followersList.get(i); //tempval will store the follwers details
-            log.info("follower " + i+1 + " = " + tempval);
+            int followerCount = i + 1;
+            PersonDto tempval = followersList.get(i); //tempval will store the followers details
+            log.info("follower " + followerCount + " = " + tempval);
+            log.info("notification sent to " + tempval.getUserName());
+
+            //write logic to send the mail  , to get the mail we need to call the feign to user srvice to get the
+            //user detail
+
+            /*
+            //produce event sendNotificationEvent
+            SendNotificationEventDto sendNotificationEventDto = new SendNotificationEventDto();
+            sendNotificationEventDto.setContent(event.getImageUrl());
+            sendNotificationEventDto.setUserId(event.getUserId());
+            sendNotificationEventDto.setUsersFollowerId(tempval.getUserId());
+
+            sendNotificationEventProducer.sendNotificationEvent(sendNotificationEventDto);
+            */
+
         }
     }
 }
