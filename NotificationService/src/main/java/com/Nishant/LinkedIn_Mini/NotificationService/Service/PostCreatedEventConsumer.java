023521fe5dbb2dc0de6322d9db735b7eba7aebac
nishant <kumarnishu574@gmail.com>
2026-02-15 01:47:52 +0530

    implemented Feign to get user info from userService

--- NotificationService/src/main/java/com/Nishant/LinkedIn_Mini/NotificationService/Service/PostCreatedEventConsumer.java
+++ NotificationService/src/main/java/com/Nishant/LinkedIn_Mini/NotificationService/Service/PostCreatedEventConsumer.java
@@ -3,7 +3,9 @@ package com.Nishant.LinkedIn_Mini.NotificationService.Service;
 import com.Nishant.LinkedIn_Mini.NotificationService.Dto.EventDto.PostCreatedEventDto;
 import com.Nishant.LinkedIn_Mini.NotificationService.Dto.EventDto.SendNotificationEventDto;
 import com.Nishant.LinkedIn_Mini.NotificationService.Dto.FeignDto.PersonDto;
+import com.Nishant.LinkedIn_Mini.NotificationService.Dto.FeignDto.UserInfoDto;
 import com.Nishant.LinkedIn_Mini.NotificationService.FeignClient.GetFollowerFeign;
+import com.Nishant.LinkedIn_Mini.NotificationService.FeignClient.GetUserInfoFeign;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static com.Nishant.LinkedIn_Mini.NotificationService.Constant.AppConstants.SUPER_USER_FOLLOWER_MIN_LIMIT;
 import static java.lang.System.in;
 
 
@@ -22,9 +25,12 @@ public class PostCreatedEventConsumer {
 
     private final  SendNotificationEventProducer sendNotificationEventProducer;
 
-    public PostCreatedEventConsumer(GetFollowerFeign getFollowerFeign, SendNotificationEventProducer sendNotificationEventProducer) {
+    private final GetUserInfoFeign getUserInfoFeign;
+
+    public PostCreatedEventConsumer(GetFollowerFeign getFollowerFeign, SendNotificationEventProducer sendNotificationEventProducer, GetUserInfoFeign getUserInfoFeign) {
         this.getFollowerFeign = getFollowerFeign;
         this.sendNotificationEventProducer = sendNotificationEventProducer;
+        this.getUserInfoFeign = getUserInfoFeign;
     }
 
 
@@ -42,6 +48,7 @@ public class PostCreatedEventConsumer {
         //now produce an event followerInfoReceivedToSendMailOrPushNotification from notification service
         //and notification service will again consume the same event to send the mail
 
+        //just for logging purpose
         for(int i = 0; i<followersList.size(); i++)
         {
             int followerCount = i + 1;
@@ -49,11 +56,38 @@ public class PostCreatedEventConsumer {
             log.info("follower " + followerCount + " = " + tempval);
             log.info("notification sent to " + tempval.getUserName());
 
-            //write logic to send the mail  , to get the mail we need to call the feign to user srvice to get the
-            //user detail
+        }
 
-            /*
-            //produce event sendNotificationEvent
+        //lets apply logic to handle the super user problem
+        if(followersList.size() < SUPER_USER_FOLLOWER_MIN_LIMIT)
+        {
+            for(int i = 0; i<followersList.size(); i++)
+            {
+
+                PersonDto person = followersList.get(i); //tempval will store the followers details
+
+                //write logic to send the mail  , to get the mail we need to call the feign to user srvice to get the
+                //user detail
+
+                //get the mail from user service using feign client
+//                UserInfoDto followerInfo =  getUserInfoFeign.GetUserInfo(person.getUserId());
+                //its working fine
+                UserInfoDto followerInfo =  getUserInfoFeign.GetUserInfo(1L , 1L);//to check if it is working or not
+                log.info("got the user info from the user service = "+ followerInfo);
+
+                //now we have email id of the user , integrate the mail service to send the mail
+
+
+
+
+
+
+            }
+
+
+        }else{
+                 /*
+            //produce event sendNotificationEvent  , making the process asynchronous for large number of followers
             SendNotificationEventDto sendNotificationEventDto = new SendNotificationEventDto();
             sendNotificationEventDto.setContent(event.getImageUrl());
             sendNotificationEventDto.setUserId(event.getUserId());
