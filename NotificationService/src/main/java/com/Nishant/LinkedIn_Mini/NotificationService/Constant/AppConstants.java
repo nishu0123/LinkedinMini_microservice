fdc7cdedfe12cb4b51e51d3a0f7e96fc345985e1
nishant <kumarnishu574@gmail.com>
2026-02-15 01:50:16 +0530

    added class to put all the constant related variable in this class

--- NotificationService/src/main/java/com/Nishant/LinkedIn_Mini/NotificationService/Constant/AppConstants.java
+++ NotificationService/src/main/java/com/Nishant/LinkedIn_Mini/NotificationService/Constant/AppConstants.java
@@ -1,4 +1,11 @@
 package com.Nishant.LinkedIn_Mini.NotificationService.Constant;
 
-public class AppConstants {
+public final class AppConstants {
+    public static final Long SUPER_USER_FOLLOWER_MIN_LIMIT = 1000L; //make it public and static so that it can be accessed from anywhere
+
+
+//private constructor to insure that this class should not be initialized
+    private AppConstants() {
+        throw new UnsupportedOperationException("This is a constant class and cannot be instantiated");
+    }
 }
