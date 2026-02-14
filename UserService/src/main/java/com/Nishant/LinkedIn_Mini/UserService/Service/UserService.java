14240f4dc55cc1987072929bc70c3a2a9831dc73
nishant <kumarnishu574@gmail.com>
2026-02-15 01:55:38 +0530

    implemented api /{userId}/getUserInfo

--- UserService/src/main/java/com/Nishant/LinkedIn_Mini/UserService/Service/UserService.java
+++ UserService/src/main/java/com/Nishant/LinkedIn_Mini/UserService/Service/UserService.java
@@ -1,4 +1,30 @@
 package com.Nishant.LinkedIn_Mini.UserService.Service;
 
+import com.Nishant.LinkedIn_Mini.UserService.Dto.UserInfoDto;
+import com.Nishant.LinkedIn_Mini.UserService.Entity.UserEntity;
+import com.Nishant.LinkedIn_Mini.UserService.Repositroy.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserInfoDto GetUserInfo(Long userId) {
+        Optional<UserEntity> userEntity = userRepository.findById(userId);
+        //map the userEntity to userInfoDto
+        return modelMapper.map(userEntity , UserInfoDto.class);
+    }
 }
