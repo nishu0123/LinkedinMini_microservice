f060e7f1fc01d71c9d0be058d10cca0c185387af
nishant <kumarnishu574@gmail.com>
2026-02-20 01:32:09 +0530

    configuration to make the sigin/login public - it will bypass the authentication for these two endpoints

--- api-gateway/src/main/java/com/Ecommerce/api_gateway/api_gateway/Config/GatewaySecurityConfig.java
+++ api-gateway/src/main/java/com/Ecommerce/api_gateway/api_gateway/Config/GatewaySecurityConfig.java
@@ -1,4 +1,32 @@
 package com.Ecommerce.api_gateway.api_gateway.Config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
 public class GatewaySecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)  // disable CSRF for APIs
+                .authorizeExchange(exchange -> exchange
+                        // Permit login & signup endpoints
+                        .pathMatchers("/api/v1/user/auth/login", "/api/v1/user/auth/signup").permitAll()
+
+                        // Allow actuator if needed
+                        .pathMatchers("/actuator/**").permitAll()
+
+                        // Everything else requires authentication
+                        .anyExchange().authenticated()
+                )
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)  // disable default basic auth popup
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)  // disable default login page
+                .build();
+    }
 }
