ad8797ef203a3eafae2d92f19bec1260493d5d06
nishant <kumarnishu574@gmail.com>
2026-04-14 01:07:06 +0530

    update discovery-service code

--- discovery-service/src/main/java/com/example/discovery_service/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/example/discovery_service/DiscoveryServiceApplication.java
@@ -1,6 +1,6 @@
 package com.example.discovery_service;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot .SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
