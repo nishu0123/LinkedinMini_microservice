ba84086a5310992085e7e06b3ddf579309a40443
Nishu kumar <nishantnishu967@gmail.com>
2026-07-03 22:02:20 +0000

    feature: implemented all the exception to return the same response which will contain the sufficient information - apiError object , custom exception class need to send only exception message and rest globalException Handler is handling the response

--- UserService/src/main/java/com/Nishant/LinkedIn_Mini/UserService/Exception/GlobalExceptionHandler.java
+++ UserService/src/main/java/com/Nishant/LinkedIn_Mini/UserService/Exception/GlobalExceptionHandler.java
@@ -1,10 +1,8 @@
 package com.Nishant.LinkedIn_Mini.UserService.Exception;
 
-import com.Nishant.LinkedIn_Mini.UserService.Dto.ErrorResponseDto;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,37 +11,89 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(InvalidCredentialsException.class)
-    public ResponseEntity<ApiError> handleInvalidCredentials(
-            InvalidCredentialsException ex,
+    private ApiError buildApiError(
+            HttpStatus status,
+            String message,
             HttpServletRequest request) {
-
         ApiError apiError = new ApiError();
 
         apiError.setTimestamp(LocalDateTime.now());
-        apiError.setStatus(HttpStatus.UNAUTHORIZED.value());
-        apiError.setError(HttpStatus.UNAUTHORIZED.getReasonPhrase());
-        apiError.setMessage(ex.getMessage());
+        apiError.setStatus(status.value());
+        apiError.setError(status.getReasonPhrase());
+        apiError.setMessage(message);
         apiError.setPath(request.getRequestURI());
+
+        return apiError;
+    }
+
+
+    @ExceptionHandler(DuplicateUserNameException.class)
+    public ResponseEntity<ApiError> handleDuplicateUserNameException(
+            DuplicateUserNameException ex,
+            HttpServletRequest request) {
+
+        ApiError apiError = buildApiError(
+                HttpStatus.CONFLICT,
+                ex.getMessage(),
+                request
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(apiError);
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ApiError> handleInvalidCredentialsException(
+            InvalidCredentialsException ex,
+            HttpServletRequest request) {
+
+        ApiError apiError = buildApiError(
+                HttpStatus.UNAUTHORIZED,
+                ex.getMessage(),
+                request
+        );
 
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(apiError);
     }
 
-    @ExceptionHandler(DuplicateUserNameException.class)
-    public ResponseEntity<ErrorResponseDto>
-    handleDuplicateUserNameException(
-            DuplicateUserNameException ex) {
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ApiError> handleUserNotFoundException(
+            UserNotFoundException ex,
+            HttpServletRequest request) {
 
-        ErrorResponseDto errorResponse =
-                new ErrorResponseDto(
-                        "DUPLICATE_USERNAME",
-                        ex.getMessage()
-                );
+        //set the status
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        ApiError apiError = buildApiError(
+                status,
+                ex.getMessage(),
+                request
+        );
 
         return ResponseEntity
-                .status(HttpStatus.CONFLICT)
-                .body(errorResponse);
+                .status(status)
+                .body(apiError);
     }
+
+
+
+
+//    @ExceptionHandler(DuplicateUserNameException.class)
+//    public ResponseEntity<ErrorResponseDto>
+//    handleDuplicateUserNameException(
+//            DuplicateUserNameException ex) {
+//
+//        ErrorResponseDto errorResponse =
+//                new ErrorResponseDto(
+//                        "DUPLICATE_USERNAME",
+//                        ex.getMessage()
+//                );
+//
+//        return ResponseEntity
+//                .status(HttpStatus.CONFLICT)
+//                .body(errorResponse);
+//    }
 }
